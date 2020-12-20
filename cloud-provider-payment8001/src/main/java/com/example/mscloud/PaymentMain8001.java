6f82f77fbf719d91bba725f768d5850aadfdf190
yao09605 <785339545@qq.com>
2020-12-20 13:14:55 +0800

    add eureka servers

--- cloud-provider-payment8001/src/main/java/com/example/mscloud/PaymentMain8001.java
+++ cloud-provider-payment8001/src/main/java/com/example/mscloud/PaymentMain8001.java
@@ -3,11 +3,13 @@ package com.example.mscloud;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author yaochenli
  * @create 2020-12-17
  */
+@EnableEurekaClient
 @SpringBootApplication
 @MapperScan(basePackages = {"com.example.mscloud.dao"})
 public class PaymentMain8001 {
