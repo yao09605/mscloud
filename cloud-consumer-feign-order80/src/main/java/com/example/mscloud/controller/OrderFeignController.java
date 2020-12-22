260fceeaec337a01c126d58028026e948ae2d529
yaochenli <785339545@qq.com>
2020-12-22 15:20:40 +0800

    Open Feign

---
+++ cloud-consumer-feign-order80/src/main/java/com/example/mscloud/controller/OrderFeignController.java
@@ -0,0 +1,20 @@
+package com.example.mscloud.controller;
+
+import com.example.mscloud.service.PaymentFeignService;
+import entities.CommonResult;
+import entities.Payment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+public class OrderFeignController {
+    @Resource
+    PaymentFeignService paymentFeignService;
+    @GetMapping(value = "/consumer/payment/get/{id}")
+    public CommonResult<Payment> getPaymentById(@PathVariable("id") Long id){
+        return paymentFeignService.getPaymentById(id);
+    }
+}
