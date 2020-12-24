90fc21cab5c8c57a2600818674bf9c3ff44bd4a6
yaochenli <785339545@qq.com>
2020-12-24 17:32:03 +0800

    end hystrix

--- cloud-consumer-feign-hystrix-order80/src/main/java/com/example/mscloud/OrderHystrixMain80.java
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/example/mscloud/OrderHystrixMain80.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+//@EnableHystrix
 public class OrderHystrixMain80 {
     public static void main(String[] args) {
         SpringApplication.run(OrderHystrixMain80.class,args);
--- cloud-consumer-feign-hystrix-order80/src/main/java/com/example/mscloud/controller/OrderHystrixController.java
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/example/mscloud/controller/OrderHystrixController.java
@@ -14,6 +14,7 @@ import javax.annotation.Resource;
 
 @RestController
 @Slf4j
+//@DefaultProperties(defaultFallback = "payment_Global_FallbackMethod")
 public class OrderHystrixController {
     @Resource
     private PaymentHystrixService paymentHystrixService;
@@ -31,10 +32,22 @@ public class OrderHystrixController {
         return result;
     }
     @GetMapping("/consumer/payment/hystrix/timeout/{id}")
+    //@HystrixCommand
+    /*(fallbackMethod = "paymentTimeOutFallbackMethod",commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds",value = "1500")  //3秒钟以内就是正常的业务逻辑
+    })*/
     public String paymentInfo_TimeOut(@PathVariable("id") Integer id){
         String result = paymentHystrixService.paymentInfo_TimeOut(id);
         log.info("*******result:"+result);
         return result;
+    }
+
+    public String paymentTimeOutFallbackMethod(@PathVariable("id") Integer id){
+        return "我是消费者80，对付支付系统繁忙请10秒钟后再试或者自己运行出错请检查自己,(┬＿┬)";
+    }
+    //下面是全局fallback方法
+    public String payment_Global_FallbackMethod(){
+        return "Global异常处理信息，请稍后再试,(┬＿┬)";
     }
 
 }
