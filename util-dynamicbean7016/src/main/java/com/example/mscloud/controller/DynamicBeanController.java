03b51a8431644001535f913ddfae3805e3c1329e
yao09605 <yao09605@163.com>
2021-01-24 22:20:58 +0800

    add DynamicBean2

--- util-dynamicbean7016/src/main/java/com/example/mscloud/controller/DynamicBeanController.java
+++ util-dynamicbean7016/src/main/java/com/example/mscloud/controller/DynamicBeanController.java
@@ -5,9 +5,16 @@ package com.example.mscloud.controller;
 import com.alibaba.fastjson.JSON;
 import com.example.mscloud.entity.LoggerEntity;
 
+import org.apache.commons.beanutils.BasicDynaBean;
+import org.apache.commons.beanutils.BasicDynaClass;
+import org.apache.commons.beanutils.DynaBean;
+import org.apache.commons.beanutils.DynaProperty;
+import org.apache.commons.beanutils.PropertyUtils;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -27,13 +34,26 @@ public class DynamicBeanController {
             put( "hello" , "world" );
             put( "abc" , "123" );
         }};
-        System.out.println("entity:"+JSON.toJSONString(entity));
-        Object obj = getTarget(entity,addProperties);
-        System.out.println("obj:"+JSON.toJSONString(obj));
-        System.out.println("obj1:"+obj.getClass());
-        System.out.println("obj2:"+obj);
-        obj = (LoggerEntity) obj ;
+        Object obj=getTarget(entity,addProperties);
+
         System.out.println("obj3:"+obj);
         //System.out.println(getTarget(entity, addProperties));
+    }
+
+    @RequestMapping("/bean2")
+    public void dynamicBean2() throws InstantiationException, IllegalAccessException, InvocationTargetException, NoSuchMethodException {
+        DynaProperty[] dynaProperties = new DynaProperty[]
+                { new DynaProperty("name", String.class),
+                        new DynaProperty("age", Integer.class) };
+        BasicDynaClass basicDynaClass = new BasicDynaClass("person",
+                BasicDynaBean.class, dynaProperties);
+        DynaBean personBean = basicDynaClass.newInstance();
+        personBean.set("name", "zhangming");
+        personBean.set("age", 123);
+        System.out.println("-----------");
+        System.out.println(personBean.get("name"));
+        System.out.println(personBean.get("age"));
+        System.out.println("......");
+        System.out.println(PropertyUtils.getSimpleProperty(personBean, "name"));
     }
 }
